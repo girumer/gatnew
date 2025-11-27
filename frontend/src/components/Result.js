e94cf211ee88442d1cda3e354d276711c0d6b267
girumer <bgirum9@gmail.com>
2025-11-27 17:46:16 +0300

    Initial commit

--- frontend/src/components/Result.js
+++ frontend/src/components/Result.js
@@ -8,6 +8,7 @@ import { resetAllAction } from '../redux/question_reducer';
 import { resetResultAction } from '../redux/result_reducer';
 import { usePublishResult } from '../hooks/setResult';
 import { useLocation } from 'react-router-dom';
+import UserResults from './UserResults';
 function Result() {
   const dispatch=useDispatch();
  const {questions:{queue,answers},result:{result,userId}}  = useSelector(state => state)
